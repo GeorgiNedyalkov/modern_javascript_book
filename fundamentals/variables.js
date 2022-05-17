40d7d45103dbab61fbc9604b63632189c538fc4b
Flu3nt <gnedyalkov94@gmail.com>
2022-05-17 12:19:13 +0300

    Learning and training about variables

--- fundamentals/variables.js
+++ fundamentals/variables.js
@@ -1,7 +1,3 @@
-"use strict";
-
-alert("I am JavaScript");
-
 // The hotkey for a single line comment is ctrl + /
 // Capital named constants are named only if the values are know prior to execution and are hard coded
 const COLOR_BLACK = "#000000";
@@ -11,3 +7,16 @@ const COLOR_LIME = "#00FF00";
 const COLOR_BLUE = "0000FF"
 const COLOR_YELLOW = "#FFFF00";
 const COLOR_GREY = "#808080";
+
+let admin = "John";
+let name;
+name = admin;
+
+alert(name);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+const BIRTHDAY_GEORGI = '09.07.1994';
+
+const age = calculateAge(BIRTHDAY_GEORGI);
