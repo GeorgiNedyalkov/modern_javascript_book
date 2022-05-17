c2bce1bcba93461a02122c59793cdd60583ef4f4
Flu3nt <gnedyalkov94@gmail.com>
2022-05-17 13:00:40 +0300

    Created a basic js file to practice intractions with a user

--- fundamentals/introduction.js
+++ fundamentals/introduction.js
@@ -2,12 +2,3 @@
 
 alert("I am JavaScript");
 
-// The hotkey for a single line comment is ctrl + /
-// Capital named constants are named only if the values are know prior to execution and are hard coded
-const COLOR_BLACK = "#000000";
-const COLOR_WHITE = "#FFFFFF";
-const COLOR_RED = "FF0000";
-const COLOR_LIME = "#00FF00";
-const COLOR_BLUE = "0000FF"
-const COLOR_YELLOW = "#FFFF00";
-const COLOR_GREY = "#808080";
