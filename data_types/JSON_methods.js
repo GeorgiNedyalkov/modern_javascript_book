e938fccac2c21550883c20060b14d9af127a6a1d
Flu3nt <gnedyalkov94@gmail.com>
2022-05-31 09:05:03 +0300

    Finished the lesson on JSON

--- data_types/JSON_methods.js
+++ data_types/JSON_methods.js
@@ -129,4 +129,97 @@ console.log(JSON.stringify(meetup));
 // let json2 = JSON.stringify(value[, replacer, space]);
 
 // value - a value to encode
-// replacer - Array of 
+// replacer - Array of properties to encore or a mapping function - function (key, value)
+// space - Amount of space to use for formatting
+// Most of the time we use, JSON.stringify with the first argument only.
+// But if we need to fine-tune the replacement process, like to filter out circular references
+// We can use the second argument of JSON.stringify
+
+let table = {
+    number: 42
+};
+
+let gathering = {
+    title: "Backroom",
+    participants: [{ name: "Geremiah" }, { name: "Alice" }],
+    place: table
+}
+
+table.occupeidBy = gathering;
+
+console.log(JSON.stringify(gathering, ['title', 'participants', 'place', 'name', 'number']));
+
+console.log(JSON.stringify(gathering, function replacer(key, value) {
+    console.log(`${key}: ${value}`);
+    return (key == 'occupiedBy') ? undefined : value;
+}));
+
+// Formatting: space
+// The third argument of JSON.stringify (space) is the number of spaces to use for pretty formatting
+// The space argument is used exclusively for a nice output.
+
+// Custom "toJSON"
+
+let livingroom = {
+    size: 50,
+    toJSON() {
+        return this.size;
+    }
+}
+
+// JSON.parse
+
+// To decode a JSON-string, we need another method JSON.parse
+
+let value = JSON.parse(str, [reviver]);
+
+// str is string to parse
+// reviver...
+// optional function(key, value) that will be called for each (key, value) pair and can transform the value
+
+let numArr = "[0, 1, 2, 3]";
+
+numArr = JSON.parse(numArr);
+
+console.log(numArr[1]);
+
+// The JSON may be as complex as necessary. Objects and arrays can include other objects and arrays.
+// But they must obey the same JSON format.
+// property names should be with quotes
+// single quotes in value are not accpeted (must be double)
+// single quotes in key are not accepted (must be double)
+// no "new" keyword is allowed, only bare values
+
+// JSON does not support comments. Adding comments to it makes it invalid.
+
+// Using reviver
+// When we stringify a Date or a nested object when we use JSON.parse
+// We get an error as it cannot by itself turn them back to objects from a string
+// Here is how we use reviver to do it
+
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+let venue = JSON.parse(str, function (key, value) {
+    if (key == 'date') return new Date(value);
+    return key;
+})
+
+// This works for nested objects as well
+// Summary
+/**
+ * JSON is a data format that has its own independent standard and libraries for most programming languages.
+ * JSON supports plain objects, arrays, strings, numbets, booleans and null
+ * JavaScript provides methods JSON.stringify to serialize into JSON and JSON.parse to read from JSON
+ * Both methods support transformer functions for smart reading/ writing
+ * If an object has toJson, then it is called by JSON.stringify
+*/
+
+// Tasks 
+// Turn the object into JSON and back
+
+let human = {
+    name: "John Smith",
+    age: 35
+};
+
+JSON.stringify(human);
